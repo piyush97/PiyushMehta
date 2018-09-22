29f8daae24bafd7d7ebe028726dd83bd406b9958
piyush mehta <me@piyushmehta.com>
2018-09-22 11:07:59 +0530

    changes
     Committer: piyush mehta <me@piyushmehta.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,18 +31,6 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'fullscreen',
-        icons: [
-          {
-            src: '/favicons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/favicons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
       },
     },
     /* Must be placed at the end */
